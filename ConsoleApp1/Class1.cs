48f5c4cdc6eeebb1f75fb47b73cc3b6dbb3583ef
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-10-23 11:14:40 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Character
+    {
+
+        public string name;
+        public int lifePoint;
+        public int force;
+        public int defense;
+
+        public void Attack(int enemyForce)
+        {
+            lifePoint = lifePoint - (enemyForce - defense);
+            Console.WriteLine(lifePoint);
+        }
+
+        public bool IsAlive()
+        {
+            if(this.lifePoint > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character Character1 = new Character();
+            Character Character2 = new Character();
+
+            Character1.lifePoint = 100;
+            Character1.force = 50;
+            Character1.name = "Perso 1";
+            Character1.defense = 10;
+
+            Character2.lifePoint = 100;
+            Character2.force = 20;
+            Character2.name = "Perso 2";
+            Character2.defense = 10;
+
+
+            while (Character1.lifePoint == 0 || Character2.lifePoint == 0)
+            {
+                Character1.Attack(Character2.force);
+                if (Character1.IsAlive())
+                {
+                    Console.WriteLine(Character1.name + "est en vie");
+                }
+                else
+                {
+                    Console.WriteLine(Character1.name + "est mort");
+                }
+
+
+                Character2.Attack(Character1.force);
+                if (Character2.IsAlive())
+                {
+                    Console.WriteLine(Character2.name + "est en vie");
+                }
+                else
+                {
+                    Console.WriteLine(Character1.name + "est mort");
+                }
+
+
+            }
+
+        }
+    }
+}
